30b3ea8ec0f1a4a15278466049e253f1b007946c
LuiiVasquez <luivasque95@gmail.com>
2022-12-26 11:23:09 -0600

    adding Main component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <Main />
     </div>
   );
 }
