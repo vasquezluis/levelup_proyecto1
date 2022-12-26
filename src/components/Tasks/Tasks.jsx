953f07c2b50ec9922cda39f286164f654aeeb71d
LuiiVasquez <luivasque95@gmail.com>
2022-12-26 14:56:58 -0600

    adding check input for toggle

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-function Tasks({ task }) {
+function Tasks({ task, toggleTask }) {
   return (
-    <div className="bg-gray-800 text-white p-4 rounded-md min-h-[100px]">
-      <h2 className="text-xl font-bold">{task.name}</h2>
+    <div
+      className={`${
+        task.done ? "bg-slate-800 opacity-40" : "bg-slate-800"
+      } text-white p-4 rounded-md min-h-[100px]`}
+    >
+      <div className="flex justify-between">
+        <h2 className="text-xl font-bold">{task.name}</h2>
+        <div>
+          <label htmlFor={task.name} className="mx-2">Done</label>
+          <input
+            id={task.name}
+            type="checkbox"
+            checked={task.done}
+            onChange={() => toggleTask(task)}
+          />
+        </div>
+      </div>
       <p>{task.description}</p>
       <div className="grid grid-cols-2 justify-center">
         <button className="bg-yellow-500 mx-2 px-2 py-1 rounded-md mt-4">
