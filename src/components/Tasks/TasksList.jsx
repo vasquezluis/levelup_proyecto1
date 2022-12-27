44892596522e73ff2cd7949b5b4f2456263cc52f
LuiiVasquez <luivasque95@gmail.com>
2022-12-27 11:50:58 -0600

    adding handleDelete

--- src/components/Tasks/TasksList.jsx
+++ src/components/Tasks/TasksList.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Tasks from "./Tasks";
 
-function TasksList({ tasks, toggleTask }) {
+function TasksList({ tasks, toggleTask, handleDelete }) {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 py-2">
-      {tasks.map((task) => (
-        // key de la tarea: nombre
-        <Tasks key={task.name} task={task} toggleTask={toggleTask}/>
-      ))}
+      {tasks
+        .map((task) => (
+          // key de la tarea: nombre
+          <Tasks
+            key={task.name}
+            task={task}
+            toggleTask={toggleTask}
+            handleDelete={handleDelete}
+          />
+        ))
+        .reverse()}
     </div>
   );
 }
