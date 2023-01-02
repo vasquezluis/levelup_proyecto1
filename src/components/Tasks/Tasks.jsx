8501f1a07b6a1cc8aa772ff0af6b2cc84fcfa19e
LuiiVasquez <luivasque95@gmail.com>
2023-01-02 14:29:26 -0600

    adding handleUpdate function

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 
-function Tasks({ task, toggleTask, handleDelete }) {
+function Tasks({ task, toggleTask, handleDelete, handleUpdate }) {
   return (
     <div
-      className={`${
+      className={`m-2 ${
         task.done ? "bg-slate-800 opacity-40" : "bg-slate-800"
-      } text-white p-4 rounded-md min-h-[100px]`}
+      } text-white p-4 rounded-md `}
     >
       <div className="flex justify-between mb-5">
-        <h2 className="text-xl font-bold">{task.name}</h2>
+        <h2
+          className={
+            task.done ? "text-xl font-bold line-through" : "text-xl font-bold"
+          }
+        >
+          {task.name}
+        </h2>
         <div>
           <label htmlFor={task.name} className="mx-2 cursor-pointer">
             Done
@@ -22,9 +28,24 @@ function Tasks({ task, toggleTask, handleDelete }) {
           />
         </div>
       </div>
-      <p className="text-slate-300">{task.description}</p>
+      <p
+        className={task.done ? "text-slate-300 line-through" : "text-slate-300"}
+      >
+        {task.description}
+      </p>
       <div className="grid grid-cols-2 justify-center">
-        <button className="bg-teal-500 py-1 rounded-md mt-4">Editar</button>
+        {task.done ? (
+          <span></span>
+        ) : (
+          <button
+            className={`bg-teal-500 py-1 rounded-md mt-4 ${
+              task.done ? "cursor-default" : ""
+            }`}
+            onClick={() => handleUpdate(task)}
+          >
+            Editar
+          </button>
+        )}
         <button
           className="bg-orange-500 mx-2 px-2 py-1 rounded-md mt-4"
           onClick={() => handleDelete(task)}
