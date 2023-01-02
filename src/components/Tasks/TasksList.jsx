ed64239103afe3bed6439cf10f8b71db5b9b5bc7
LuiiVasquez <luivasque95@gmail.com>
2023-01-02 14:30:47 -0600

    adding handleUpdate function

--- src/components/Tasks/TasksList.jsx
+++ src/components/Tasks/TasksList.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Tasks from "./Tasks";
 
-function TasksList({ tasks, toggleTask, handleDelete }) {
+function TasksList({ tasks, toggleTask, handleDelete, handleUpdate }) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 py-2">
+    <div className="grid grid-cols-4 ">
       {tasks
         .map((task) => (
           // key de la tarea: nombre
           <Tasks
-            key={task.name}
+            key={task.id}
             task={task}
             toggleTask={toggleTask}
             handleDelete={handleDelete}
+            handleUpdate={handleUpdate}
           />
         ))
         .reverse()}
