c58fc14abb5035894dc7f9148c5c851fb2faea0a
LuiiVasquez <luivasque95@gmail.com>
2023-01-03 16:56:49 -0600

    creating routing path and protected home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import "./App.css";
-import Main from "./components/Main/Main";
+
+import { Route, Routes } from "react-router-dom";
+
+import UserHome from "./pages/UserHome";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+import { AuthProvider } from "./context/authContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App">
-      <Main />
+    <div className="bg-slate-300 h-screen text-black flex">
+      <AuthProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <UserHome />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AuthProvider>
     </div>
   );
 }
