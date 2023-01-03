93e85203ccce3753936f6b4e236a3cd937eb0acb
LuiiVasquez <luivasque95@gmail.com>
2023-01-03 16:59:21 -0600

    adding firebase connection

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBY-EXI6sFcPX7ddWG-943bgwcCB2fHCkc",
+  authDomain: "levelup-proyecto1.firebaseapp.com",
+  projectId: "levelup-proyecto1",
+  storageBucket: "levelup-proyecto1.appspot.com",
+  messagingSenderId: "872806607730",
+  appId: "1:872806607730:web:224bcdad0ac126b40c8beb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
