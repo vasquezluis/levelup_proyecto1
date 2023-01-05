8228d474e2d2928302c0e526d60ff6db5ba2fcf8
LuiiVasquez <luivasque95@gmail.com>
2023-01-05 12:37:51 -0600

    changing background color

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
-    <div className="bg-slate-300 h-screen text-black flex">
+    <div className="bg-zinc-900 h-screen text-black flex">
       <AuthProvider>
         <Routes>
           <Route
