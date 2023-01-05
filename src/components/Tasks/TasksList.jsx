8264e4906e28990f6406477259ffce7b80ff4bc2
LuiiVasquez <luivasque95@gmail.com>
2023-01-05 12:43:15 -0600

    adding user filter to tasks list

--- src/components/Tasks/TasksList.jsx
+++ src/components/Tasks/TasksList.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 import Tasks from "./Tasks";
 
-function TasksList({ tasks, toggleTask, handleDelete, handleUpdate }) {
+function TasksList({ user, tasks, toggleTask, handleDelete, handleUpdate }) {
+  const currentUserTasks = tasks.filter((item) => item.user === user);
+
+  console.log(
+    `lista de tareas de ${user}: ${JSON.stringify(currentUserTasks)}`
+  );
+
   return (
-    <div className="grid grid-cols-4 ">
-      {tasks
-        .map((task) => (
-          // key de la tarea: nombre
-          <Tasks
-            key={task.id}
-            task={task}
-            toggleTask={toggleTask}
-            handleDelete={handleDelete}
-            handleUpdate={handleUpdate}
-          />
-        ))
-        .reverse()}
+    <div className="grid md:grid-cols-2 lg:grid-cols-4">
+      {currentUserTasks.length !== 0 ? (
+        currentUserTasks
+          .map((task) => (
+            // key de la tarea: nombre
+            <Tasks
+              key={task.id}
+              task={task}
+              toggleTask={toggleTask}
+              handleDelete={handleDelete}
+              handleUpdate={handleUpdate}
+            />
+          ))
+          .reverse()
+      ) : (
+        <div className="px-2 pt-6 pb-8 mb-4">
+          <h2 className="block text-3xl text-white font-bold mb-2">
+            No tienes tareas aun
+          </h2>
+        </div>
+      )}
     </div>
   );
 }
