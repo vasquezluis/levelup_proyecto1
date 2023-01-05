01f32bf73c85152969677a24f59699dfa515af39
LuiiVasquez <luivasque95@gmail.com>
2023-01-05 14:11:57 -0600

    adding styles to inputs & labels

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 
 // obtener useAuth del context
 import { useAuth } from "../context/authContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import Alert from "../components/Alert";
 
 function Login() {
   // obtener datos del usuario por el formulario
@@ -15,7 +16,12 @@ function Login() {
   const navigate = useNavigate();
 
   // estado para el manejo de errores
-  const [error, setError] = useState();
+  const [error, setError] = useState("");
+
+  // guardar datos en el estado
+  const handleChange = ({ target: { name, value } }) => {
+    setuser({ ...user, [name]: value });
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -29,37 +35,64 @@ function Login() {
       // redireccionar al home luego del registro
       navigate("/");
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
   };
 
-  // guardar datos en el estado
-  const handleChange = ({ target: { name, value } }) => {
-    setuser({ ...user, [name]: value });
-  };
-
   return (
-    <div>
-      {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          placeholder="Your email@company.ltd"
-          onChange={handleChange}
-        />
+    <div className="w-full max-w-xs m-auto">
+      {error !== null || undefined ? <Alert message={error} /> : null}
+      <div className="bg-slate-800 text-white px-8 pt-6 pb-4 pr-4">
+        <p className="text-2xl w-dull block font-bold">LOGIN</p>
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="bg-slate-800 shadow-md px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-white text-sm font-bold mb-2"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your email@company.ltd"
+            onChange={handleChange}
+          />
+        </div>
 
-        <label htmlFor="passwod">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          placeholder="******"
-          onChange={handleChange}
-        />
+        <div className="mb-4">
+          <label
+            htmlFor="passwod"
+            className="block text-white text-sm font-bold mb-2"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="******"
+            onChange={handleChange}
+          />
+        </div>
 
-        <button>Login</button>
+        <p className="my-4 text-sm flex justify-between px-3 text-white">
+          No tienes una cuenta? <Link to="/register" className="font-bold">Register</Link>
+        </p>
+
+        <button
+          type="submit"
+          className="bg-teal-600 hover:bg-teal-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Login
+        </button>
       </form>
     </div>
   );
