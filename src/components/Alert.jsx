0f498dd4e33398e991ff1869a7725e2146fa2425
LuiiVasquez <luivasque95@gmail.com>
2023-01-08 15:30:19 -0600

    adding tailwind classes to alert

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,7 +1,7 @@
 function Alert({message}) {
   return (
-    <div>
-      <span>{message}</span>
+    <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-2 text-center">
+      <span className="sm:inline block ">{message}</span>
     </div>
   )
 }
